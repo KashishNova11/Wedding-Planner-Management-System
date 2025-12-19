f8986c654ac86b4f779ed0e9ed048ba540d6f48b
Kashish Katakpara <kashishkatakpara30.11@gmail.com>
2025-12-19 10:31:58 +0530

    Add files via upload

---
+++ Instagram/instagram.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>
+            View On Map
+        </title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <div class="error">
+            <div class="humster">
+                <img src="img/humster.png" alt="error" class="img">
+                <h3 class="error-msg">
+                    Connect to The Internet
+                </h3>
+                <p class="error-msg-2">
+                    You're Offline. Check Your Connection
+                </p>
+            </div>
+        </div>
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
